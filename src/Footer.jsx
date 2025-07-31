d9b2b8d372c1573185f6f93f8a4f7d1630f3bc94
gspeg <105271853+gspeg@users.noreply.github.com>
2025-07-31 17:26:47 +0200

    path immagini

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,10 @@ function Footer() {
       <footer className="mt-20 py-10 border-t border-white text-center text-sm flex flex-col gap-4 items-center">
         <div className="flex gap-6">
           <a href="https://www.instagram.com/giorgio_spegis/following/" target="_blank">
-            <img src="media/images/Negative.png" className="w-6 h-6" alt="Instagram" />
+            <img src="public/media/images/Negative.png" className="w-6 h-6" alt="Instagram" />
           </a>
           <a href="https://www.linkedin.com/in/giorgio-spegis-119200322/" target="_blank">
-            <img src="media/images/NegativeL.png" className="w-6 h-6" alt="LinkedIn" />
+            <img src="public/media/images/NegativeL.png" className="w-6 h-6" alt="LinkedIn" />
           </a>
           <a class= "font-sans font-extralight" href="tel:+393451230890">📞: +393451230890</a>
           <a class= "font-sans font-extralight" href="giorgio.spegis25@gmail.com">✉️: giorgio.spegis25@gmail.com</a>
