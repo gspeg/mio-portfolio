1acfdf90bff9db26d3bdb83e9a87096e1e4922db
gspeg <105271853+gspeg@users.noreply.github.com>
2025-07-31 18:03:26 +0200

    aa

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/mio-portfolio/'
-=======
-  base: '/mio-portfolio_2/'
->>>>>>> d4e30d9f091eced0d4b8c1add59d8a96908c1252
 })
